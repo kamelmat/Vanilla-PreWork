141f3ecd2c173d081d4af0fd209e2678bb3dc924
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-10-21 17:54:16 +0000

    [Añadido de la funcion y todo el proyecto acabado] index.html: Se le añade el script y el id de excusa app.js: Se le añado la funcion y los strings

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const indexWho = Math.floor(Math.random() * who.length);
+  const indexAction = Math.floor(Math.random() * action.length);
+  const indexWhat = Math.floor(Math.random() * what.length);
+  const indexWhen = Math.floor(Math.random() * when.length);
+
+  const excuseRandom = `${who[indexWho]} ${action[indexAction]} ${what[indexWhat]} ${when[indexWhen]}`;
+
+  //Cambiamos el id excuse del html
+  document.getElementById("excuse").innerText = excuseRandom;
 };
+
+//Variables con los strings
+let who = ["Mi perro", "Jose", "Luis", "La clase 84"];
+let action = ["se ha comido", "escondió", "destruyo", "me hizo chantaje con"];
+let what = ["mi proyecto de excusas", "mi Ford Mustang", "mi tarjeta gráfica"];
+let when = [
+  "mientras veía One Piece",
+  "mientras dormía",
+  "mientras estudiaba Javascript",
+  "durante una carrera de caracoles",
+  "durante Eurovisión",
+];
