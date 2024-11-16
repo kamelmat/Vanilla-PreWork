07297e24ca2040bae9726e046b5997aebe3bcc01
chrisOkeith01 <christopherkeith70@gmail.com>
2024-11-15 19:25:33 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
 window.onload = function() {
   //write your code here
