0040a4bb8abcf6c014ba8cdc0d4007345ed8ac33
Jaime BP <jaimebernet@gmail.com>
2024-11-18 14:20:13 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let subjects = ["Mi perro", "Mi abuela", "Un extraterrestre", "El vecino"];
+  let actions = ["se comió", "perdió", "rompió", "escondió"];
+  let objects = ["mis deberes", "las llaves", "mi cartera", "el coche"];
+  let places = ["en el parque", "en la oficina", "en la playa", "en casa"];
+
+  let getRandomElement = array =>
+    array[Math.floor(Math.random() * array.length)];
+
+  let generateExcuse = () => {
+    let subject = getRandomElement(subjects);
+    let action = getRandomElement(actions);
+    let object = getRandomElement(objects);
+    let place = getRandomElement(places);
+
+    return `${subject} ${action} ${object} ${place}.`;
+  };
+
+  document.querySelector(".excuse").innerText = generateExcuse();
 };
