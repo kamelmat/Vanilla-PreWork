ffad441a3ea3852ad80f4e482a23cc776c377ccb
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2025-01-20 20:42:05 +0000

    Feature Work with Spain-98 at 4Geeks January 20th

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generarExcusa();
+  });
+};
+
+let generarExcusa = () => {
+  let sujeto = [
+    "El Perro",
+    "El Gato",
+    "El Pingüino",
+    "Mi Abuela",
+    "El Profesor",
+    "El anario",
+  ];
+  let accion = ["se comio", "se llevo", "rompio", "mordio", "escondio", "boto"];
+  let objeto = [
+    "mis papeles",
+    "mi telefono",
+    "mi coche",
+    "mi tarea",
+    "mis zapatillas",
+  ];
+  let cuando = [
+    "esta mañana",
+    "ayer por la tarde",
+    "anoche",
+    "hoy al mediodia",
+    "Ayer a la noche",
+    "No me acuerdo cuando paso",
+  ];
+
+  let arrayDeExcusas = [sujeto, accion, objeto, cuando];
+
+  let excusa = arrayDeExcusas.map((arr) => {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  });
+
+  return excusa.join(" ");
 };
