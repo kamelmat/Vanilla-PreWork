a639c800bb56ec8c8e0b6881e0694efe7f66726d
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2025-01-22 20:37:55 +0000

    Januray 22nd class, Meme Generator

--- src/app.js
+++ src/app.js
@@ -1,48 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const phrases = [
+  "Cuando te das cuenta que es lunes otra vez...",
+  "Yo esperando el fin de semana como...",
+  "No estaba preparado para esto.",
+  "Cuando tu código no funciona y no sabes por qué.",
+  "¿Por qué no me sale el examen si estudié?"
+];
 
-window.onload = function() {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excusa").innerHTML = generarExcusa();
-  });
-};
+// 1 - buscar y ligar los elementos de HTML. hay que crear variables y getEelementByid de todos los ids
 
-let generarExcusa = () => {
-  let sujeto = [
-    "El Perro",
-    "El Gato",
-    "El Pingüino",
-    "Mi Abuela",
-    "El Profesor",
-    "El anario",
-  ];
-  let accion = ["se comio", "se llevo", "rompio", "mordio", "escondio", "boto"];
-  let objeto = [
-    "mis papeles",
-    "mi telefono",
-    "mi coche",
-    "mi tarea",
-    "mis zapatillas",
-  ];
-  let cuando = [
-    "esta mañana",
-    "ayer por la tarde",
-    "anoche",
-    "hoy al mediodia",
-    "Ayer a la noche",
-    "No me acuerdo cuando paso",
-  ];
+const imageSelect = document.getElementById("image-select");
+const generateButton = document.getElementById("generate");
+const customTextInput = document.getElementById("custom-text");
+const memeImage = document.getElementById("meme-image");
+const memeText = document.getElementById("meme-text");
 
-  let arrayDeExcusas = [sujeto, accion, objeto, cuando];
+// Hacer una función que cambie la imagen al seleccionar una nueva imagen del desplegable.
+imageSelect.addEventListener("change", () => {
+  const selectedImage = imageSelect.value;
+  memeImage.src = selectedImage;
+});
 
-  let excusa = arrayDeExcusas.map((arr) => {
-    let randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  });
+// Generar una función que genere (elija del array) una frase aleatoria
 
-  return excusa.join(" ");
-};
+generateButton.addEventListener("click", () => {
+  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  memeText.textContent = randomPhrase;
+});
+
+// Actualizar el texto del meme con el input del usuario
+
+customTextInput.addEventListener("input", event => {
+  memeText.textContent = event.target.value;
+});
+
+//Actualizar la imagen y el texto en la zona del meme
